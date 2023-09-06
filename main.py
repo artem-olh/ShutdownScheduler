db2aac79b8a08babfa71ebd53ad58ad743277125
artem-olh <kosta.saimpson@mail.ru>
2023-09-07 02:19:27 +0500

    Программа которая отключает компьютер  в заданное время, за 5 мин до заданного времени заходит в бразузер chrome  нажимает кнопку на сайте и закрывает сайт и выключает комп в заданное время

--- main.py
+++ main.py
@@ -1,16 +1,60 @@
-# This is a sample Python script.
+import tkinter as tk
+from tkinter import messagebox
+import subprocess
+import time
+import os
+from selenium import webdriver
+import threading
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def background_task():
+    shutdown_time = entry.get()
+    try:
+        hours, minutes = map(int, shutdown_time.split(':'))
+        current_time = time.localtime()
+        current_hours = current_time.tm_hour
+        current_minutes = current_time.tm_min
+        seconds_until_shutdown = (hours - current_hours) * 3600 + (minutes - current_minutes) * 60
 
+        if seconds_until_shutdown <= 0:
+            messagebox.showerror("Ошибка", "Время для выключения уже прошло или некорректно.")
+        else:
+            messagebox.showinfo("Установка выключения",
+                                f"Компьютер будет выключен через {seconds_until_shutdown // 3600} часов {seconds_until_shutdown % 3600 // 60} минут.")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+            chrome_driver_path = 'C:\Program Files\Google\Chrome\Application\chromedriver.exe'
+            os.environ['webdriver.chrome.driver'] = chrome_driver_path
+            driver = webdriver.Chrome()
+            driver.get('https://www.youtube.com/')
+            element = driver.find_element('css selector', 'a#video-title-link')
+            element.click()
+            time.sleep(20)
+            driver.quit()
+            time.sleep(seconds_until_shutdown - 300)  # Wait 5 minutes (300 seconds) before shutdown
+            subprocess.run(["shutdown", "/s", "/t", "0"])
+    except ValueError:
+        messagebox.showerror("Ошибка", "Некорректный формат времени.")
 
+def set_shutdown_time():
+    thread = threading.Thread(target=background_task)
+    thread.start()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def stop_execution():
+    app.destroy()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+app = tk.Tk()
+app.title("Автоматическое выключение компьютера")
+app.geometry("400x200")
+
+label = tk.Label(app, text="Укажите время для автоматического выключения (ЧЧ:ММ):")
+label.pack(anchor='center', pady=10)
+
+entry = tk.Entry(app)
+entry.pack(anchor='center', pady=5)
+
+start_button = tk.Button(app, text="Установить выключение", command=set_shutdown_time)
+start_button.pack(anchor='center',pady=5)
+
+stop_button = tk.Button(app, text="Прекратить работу", command=stop_execution)
+stop_button.pack(anchor='center')
+
+app.mainloop()
